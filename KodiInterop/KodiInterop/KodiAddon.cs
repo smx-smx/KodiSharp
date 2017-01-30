5c9a49317c2ee75a71142a9658684cebff6ba2dc
smx-smx <smxdev4@gmail.com>
2017-01-30 16:37:30 +0100

    Moved some common methods to abstract KodiAddon

---
+++ KodiInterop/KodiInterop/KodiAddon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Smx.KodiInterop
+{
+    public abstract class KodiAddon
+    {
+		//http://stackoverflow.com/a/1373295
+		private static Assembly LoadFromSameFolder(object sender, ResolveEventArgs args) {
+			string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+			if (!File.Exists(assemblyPath)) return null;
+			Assembly assembly = Assembly.LoadFrom(assemblyPath);
+			return assembly;
+		}
+
+		private static void SetAssemblyResolver() {
+			AppDomain currentDomain = AppDomain.CurrentDomain;
+			currentDomain.AssemblyResolve += new ResolveEventHandler(LoadFromSameFolder);
+		}
+
+		public KodiAddon() {
+			SetAssemblyResolver();
+		}
+
+		public int Run() {
+			try {
+				return this.PluginMain();
+			} catch (Exception ex) {
+				// This takes the exception and stores it, not allowing it to bubble up
+				KodiBridge.SaveException(ex);
+				return 1;
+			}
+		}
+
+		public abstract int PluginMain();
+    }
+}
--- KodiInterop/TestPlugin/TestPlugin.cs
+++ KodiInterop/TestPlugin/TestPlugin.cs
@@ -12,40 +12,17 @@ using System.Reflection;
 using System.IO;
 
 namespace TestPlugin {
-	public class TestPlugin
+	public class TestPlugin : KodiAddon
     {
-		private static string PluginPath = null;
-
-		[DllExport("PluginInit", CallingConvention = CallingConvention.Cdecl)]
-		public static void PluginInit(string pluginPath) {
-			PluginPath = pluginPath;
-		}
-
 		private static void testException() {
 			throw new Exception("I should appear in kodi.log");
-		}
-
-		//http://stackoverflow.com/a/1373295
-		static Assembly LoadFromSameFolder(object sender, ResolveEventArgs args) {
-			string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-			string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
-			if (!File.Exists(assemblyPath)) return null;
-			Assembly assembly = Assembly.LoadFrom(assemblyPath);
-			return assembly;
-		}
-
-		static void SetAssemblyResolver() {
-			AppDomain currentDomain = AppDomain.CurrentDomain;
-			currentDomain.AssemblyResolve += new ResolveEventHandler(LoadFromSameFolder);
 		}
 
 		/// <summary>
 		/// Plugin Main Logic
 		/// </summary>
 		/// <returns></returns>
-		public static int Main() {
-			SetAssemblyResolver();
-
+		public override int PluginMain() {
 			ConsoleHelper.CreateConsole();
 			Console.WriteLine("TestPlugin v1.0 - Smx");
 
@@ -65,15 +42,8 @@ namespace TestPlugin {
 		}
 
 		[DllExport("PluginMain", CallingConvention = CallingConvention.Cdecl)]
-		public static int PluginMain() {
-			try {
-				return Main();
-			}
-			catch(Exception ex) {
-				// This takes the exception and stores it, not allowing it to bubble up
-				KodiBridge.SaveException(ex);
-				return 1;
-			}
+		public static int Main() {
+			return new TestPlugin().Run();
 		}
 	}
 }
