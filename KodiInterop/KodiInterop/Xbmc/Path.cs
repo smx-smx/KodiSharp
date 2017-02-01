f555dc7ccb4876ba7ebb9b6a8bd563ad3e770019
smx-smx <smxdev4@gmail.com>
2017-02-01 20:25:51 +0100

    Implemented some Path functions

---
+++ KodiInterop/KodiInterop/Xbmc/Path.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smx.KodiInterop.Xbmc
+{
+	public static class Path {
+		/// <summary>
+		/// Returns a thumb cache filename.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string getCacheThumbName(string path) {
+			return PythonInterop.CallFunction(
+				new PythonFunction(PyModules.Xbmc, "getCacheThumbName"),
+				new List<object> { path }
+			);
+		}
+
+		/// <summary>
+		/// Returns a legal filename or path as a string.
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <param name="fatX"></param>
+		/// <returns></returns>
+		public static string makeLegalFilename(string filename, bool fatX = false) {
+			return PythonInterop.CallFunction(
+				new PythonFunction(PyModules.Xbmc, "makeLegalFilename"),
+				new List<object> { filename, fatX }
+			);
+		}
+
+		/// <summary>
+		/// Returns the translated path.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string translatePath(string path) {
+			return PythonInterop.CallFunction(
+				new PythonFunction(PyModules.Xbmc, "translatePath"),
+				new List<object> { path }
+			);
+		}
+
+		/// <summary>
+		/// Returns the validated path.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static string validatePath(string path) {
+			return PythonInterop.CallFunction(
+				new PythonFunction(PyModules.Xbmc, "validatePath"),
+				new List<object> { path }
+			);
+		}
+	}
+}
