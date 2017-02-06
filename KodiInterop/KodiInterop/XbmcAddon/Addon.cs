e76ca2fbb37bd1821d2471a9d34d5d1b6b60a885
smx-smx <smxdev4@gmail.com>
2017-02-06 20:25:43 +0100

    Implemented XbmcAddon

---
+++ KodiInterop/KodiInterop/XbmcAddon/Addon.cs
@@ -0,0 +1,69 @@
+﻿using Smx.KodiInterop;
+using Smx.KodiInterop.Python;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XbmcAddon
+{
+    public class Addon
+    {
+		public readonly PyVariable Instance = PyVariableManager.NewVariable(isObject: true);
+
+		/// <summary>
+		/// Creates a new AddOn class.
+		/// </summary>
+		/// <param name="id">id of the addon as specified in addon.xml</param>
+		public Addon(int id) {
+			Instance.CallAssign(
+				new PythonFunction(PyModule.XbmcAddon, "Addon"),
+				new List<object> { id }
+			);
+		}
+
+		/// <summary>
+		/// Returns the value of an addon property as a string.
+		/// </summary>
+		/// <param name="info">the property that the module needs to access.</param>
+		/// <returns></returns>
+		public string GetAddonInfo(AddonInfo info) {
+			return Instance.CallFunction(
+				new PythonFunction("getAddonInfo"),
+				new List<object> { info.GetString() }
+			);
+		}
+
+		/// <summary>
+		/// Returns the value of a setting as a unicode string.
+		/// </summary>
+		/// <param name="setting">the setting that the module needs to access.</param>
+		/// <returns></returns>
+		public string GetSetting(string setting) {
+			return Instance.CallFunction(
+				new PythonFunction("getSetting"),
+				new List<object> { setting }
+			);
+		}
+
+		/// <summary>
+		/// Sets a script setting.
+		/// </summary>
+		/// <param name="setting">the setting that the module needs to access.</param>
+		/// <param name="value">value of the setting.</param>
+		public void SetSetting(string setting, object value) {
+			Instance.CallFunction(
+				new PythonFunction("setSetting"),
+				new List<object> { setting, value }
+			);
+		}
+
+		/// <summary>
+		/// Opens this scripts settings dialog.
+		/// </summary>
+		public void OpenSettings() {
+			Instance.CallFunction(
+				new PythonFunction("openSettings")
+			);
+		}
+    }
+}
---
+++ KodiInterop/KodiInterop/XbmcAddon/AddonInfo.cs
@@ -0,0 +1,36 @@
+﻿using Smx.KodiInterop;
+
+namespace XbmcAddon
+{
+	public enum AddonInfo
+	{
+		[StringValue("author")]
+		Author = 0,
+		[StringValue("changelog")]
+		Changelog,
+		[StringValue("description")]
+		Description,
+		[StringValue("disclaimer")]
+		Disclaimer,
+		[StringValue("fanart")]
+		Fanart,
+		[StringValue("icon")]
+		Icon,
+		[StringValue("id")]
+		Id,
+		[StringValue("name")]
+		Name,
+		[StringValue("path")]
+		Path,
+		[StringValue("profile")]
+		Profile,
+		[StringValue("stars")]
+		Stars,
+		[StringValue("summary")]
+		Summary,
+		[StringValue("type")]
+		Type,
+		[StringValue("version")]
+		Version
+	}
+}
