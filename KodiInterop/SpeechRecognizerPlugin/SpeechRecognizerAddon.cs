1260d191ced9ce7551cfddbabdfd8917d507a91b
Stefano Moioli <smxdev4@gmail.com>
2019-05-23 00:35:47 +0200

    Fixed SpeechRecognizerPlugin and unified build paths

--- KodiInterop/SpeechRecognizerPlugin/SpeechRecognizerAddon.cs
+++ KodiInterop/SpeechRecognizerPlugin/SpeechRecognizerAddon.cs
@@ -1,48 +1,108 @@
-﻿using RGiesecke.DllExport;
-using Smx.KodiInterop;
+﻿using Smx.KodiInterop;
 using Smx.KodiInterop.Builtins;
 using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Net;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
+using Windows.Data.Json;
 using Windows.Media.SpeechRecognition;
 
 namespace SpeechRecognizerAddon
 {
 	public class SpeechRecognizerAddon : KodiAddon
     {
-		public async Task<SpeechRecognitionResult> RecognizeSpeech() {
-			var speechRecognizer = new SpeechRecognizer();
-			await speechRecognizer.CompileConstraintsAsync();
-			SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeAsync();
-			return speechRecognitionResult;
-		}
+		public override string PluginId => "plugin.video.test";
+		private static string ExitKeyword = Translate("exit", "en").Result;
 
-		public override int PluginMain() {
-			ConsoleHelper.CreateConsole();
+		private ManualResetEvent exitEvent = new ManualResetEvent(false);
+
+		public async void RecognizeSpeech() {
+			var speechRecognizer = new SpeechRecognizer(SpeechRecognizer.SystemSpeechLanguage);
+
+			var dictationConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "dictation");
+			speechRecognizer.Constraints.Add(dictationConstraint);
+
+			var srcpres = await speechRecognizer.CompileConstraintsAsync();
+			if (srcpres.Status != SpeechRecognitionResultStatus.Success) {
+				Console.WriteLine("Failed to compile constraints");
+				exitEvent.Set();
+				return;
+			}
 
 			while (true) {
-				Console.WriteLine(" == Recognizing == ");
-				Task<SpeechRecognitionResult> recognizer = RecognizeSpeech();
-				SpeechRecognitionResult result = recognizer.Result;
-				if(result == null) {
-					Console.WriteLine("Couldn't recognize, try again");
-					continue;
+				var res = await speechRecognizer.RecognizeAsync();
+				switch (res.Status) {
+					case SpeechRecognitionResultStatus.Success:
+						break;
+					default:
+						Console.WriteLine($"Failed ({res.Status.ToString()}), try again");
+						continue;
 				}
 
-				if (result.Text == "exit")
-					break;
+				switch (res.Confidence) {
+					case SpeechRecognitionConfidence.Low:
+					case SpeechRecognitionConfidence.Rejected:
+						Console.WriteLine("Not enough confidence...");
+						continue;
+				}
 
 				UiBuiltins.Notification(
 					header: "Text spoken",
-					message: result.Text
+					message: res.Text
 				);
+
+				if (res.Text == ExitKeyword) {
+					exitEvent.Set();
+					break;
+				}
 			}
+		}
+
+		public static async Task<string> Translate(string text, string sourceLang) {
+			string uri = "https://translate.googleapis.com/translate_a/single?" +
+				"client=gtx" +
+				"&dt=t" +
+				// source language
+				"&sl=" + sourceLang +
+				// target language
+				"&tl=" + CultureInfo.CurrentUICulture.TwoLetterISOLanguageName +
+				// query
+				"&q=" + HttpUtility.UrlEncode(text);
+
+			HttpWebRequest req = HttpWebRequest.CreateHttp(uri);
+			req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
+
+			WebResponse resp = await req.GetResponseAsync();
+			using (Stream strm = resp.GetResponseStream())
+			using (StreamReader rdr = new StreamReader(strm)) {
+				string data = rdr.ReadToEnd();
+				JsonArray values = JsonArray.Parse(data);
+				return values.GetArrayAt(0).GetArrayAt(0).GetStringAt(0);
+			}
+
+		}
+
+		[Route("/")]
+		public int PluginMain(NameValueCollection parameters) {
+			ConsoleHelper.CreateConsole();
+
+			Console.WriteLine(" == Recognizing == ");
+			Console.WriteLine($"Say \"{ExitKeyword}\" to exit");
+			UiBuiltins.Notification("Info", $"Say \"{ExitKeyword}\" to exit");
+			RecognizeSpeech();
+
+			exitEvent.WaitOne();
 			return 0;
 		}
 
-		[DllExport("PluginMain", CallingConvention = CallingConvention.Cdecl)]
-		public static int Main() {
-			return new SpeechRecognizerAddon().Run();
+		[PluginEntry]
+		public static int PluginMain() {
+			return GetInstance<SpeechRecognizerAddon>(typeof(SpeechRecognizerAddon), enableDebug: true, persist: true).Run();
 		}
 	}
 }
--- default.py
+++ default.py
@@ -6,31 +6,65 @@ import os
 import sys
 from csharp.bridge import Bridge
 
+def getTestPlugin():
+	return {
+		'config': 'Debug',
+		'dir' : os.path.join('KodiInterop', 'TestPlugin', 'bin'),
+		'file': 'TestPlugin.dll'
+	}
+
+def getSpeechRecognizerPlugin():
+	return {
+		'config': 'Debug',
+		'dir': os.path.join('KodiInterop', 'SpeechRecognizerPlugin', 'bin'),
+		'file': 'SpeechRecognizerPlugin.dll'
+	}
+
+def getClrHost():
+	return {
+		'arch': 'x64',
+		'config': 'Debug',
+		'dir': os.path.join('KodiInterop', 'CLRHost', 'bin'),
+		'file': 'CLRHost.dll'
+	}
+
+def getNativeLibSuffix():
+	if sys.platform.startswith('win'):
+		return 'dll'
+	
+	if 'darwin' in sys.platform:
+		return 'dylib'
+
+	return 'so'
+
+def getMonoHost():
+	return {
+		'dir': os.path.join('KodiInterop', 'Mono', 'build'),
+		'file': 'libMonoHost.{}'.format(getNativeLibSuffix())
+	}
+
+def getPath(me, obj):
+	return os.path.join(me,
+		obj.get('dir', ''),
+		obj.get('arch', ''),
+		obj.get('config', ''),
+		obj.get('file', '')
+	)
+
+
 #### Config
+mono_on_windows = False
+enable_debug = False
+
 me = os.path.abspath(os.path.dirname(__file__))
 
-build_arch = "x64"
-build_type = "Debug"
+assembly_path = getPath(me, getTestPlugin())
+#assembly_path = getPath(me, getSpeechRecognizerPlugin())
 
-assembly_path = os.path.join(me, "KodiInterop", "TestPlugin", "bin", build_arch, build_type, "TestPlugin.dll")
-
-mono_on_windows = False
-if not "win" in sys.platform or mono_on_windows:
-	if sys.platform == "darwin":
-		nativeLibSuffix = "dylib"
-	elif "win" in sys.platform:
-		nativeLibSuffix = "dll"
-	else:
-		nativeLibSuffix = "so"
-
-	monoLibName = "libMonoHost.{}".format(nativeLibSuffix)
-	monohost_path = os.path.join(me, "KodiInterop", "Mono", "build", monoLibName)
-	lib_path = monohost_path
+if sys.platform.startswith('win') and not mono_on_windows:
+	lib_path = getPath(me, getClrHost())
 else:
-	clrhost_path = os.path.join(me, "KodiInterop", "bin", build_arch, build_type, "CLRHost.dll")
-	lib_path = clrhost_path
-
-
-
-bridge = Bridge(lib_path, assembly_path, enable_debug=False)
+	lib_path = getPath(me, getMonoHost())
+	
+bridge = Bridge(lib_path, assembly_path, enable_debug)
 bridge.run()
