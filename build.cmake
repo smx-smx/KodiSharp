40a4fbc65f70cd59a6235ba732ee9ed3cb8932d9
Stefano Moioli <smxdev4@gmail.com>
2025-02-04 01:53:24 +0100

    build.cmake: add addon ZIP creation

--- build.cmake
+++ build.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.18 FATAL_ERROR)
 
 list(INSERT CMAKE_MODULE_PATH 0 "${CMAKE_CURRENT_LIST_DIR}/cmake/msbuild2cmake/cmake")
 include(msbuild2cmake)
@@ -7,7 +7,20 @@ if(NOT DEFINED CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Debug)
 endif()
 
-set(build_dir ${CMAKE_CURRENT_LIST_DIR}/build)
+set(addon_name "plugin.video.test")
+
+execute_process(
+    COMMAND ${CMAKE_COMMAND} -E create_symlink
+        ${CMAKE_CURRENT_LIST_DIR}
+        ${CMAKE_CURRENT_LIST_DIR}/${addon_name}
+    RESULT_VARIABLE retcode
+)
+if(NOT ${retcode} EQUAL 0)
+    message(FATAL_ERROR "failed to create symlink for ZIP creation")
+endif()
+
+
+set(build_dir ${CMAKE_CURRENT_LIST_DIR}/${addon_name}/build)
 
 invoke_cmake(
     BUILD_DIR ${build_dir}
@@ -26,3 +39,14 @@ execute_process(
 if(NOT ${retcode} EQUAL 0)
     message(FATAL_ERROR "cmake --install failed with exit code ${retcode}")
 endif()
+
+file(ARCHIVE_CREATE
+    OUTPUT "plugin.video.test.zip"
+    FORMAT "zip"
+    VERBOSE
+    PATHS
+        ${build_dir}/out
+        ${addon_name}/addon.xml
+        ${addon_name}/csharp
+        ${addon_name}/default.py
+)
